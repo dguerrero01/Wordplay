744d1029bf94e9c73b34f808901cb9d82ea30228
Daisy Guerrero <dguerrero1@bsd220.org>
2021-09-29 10:39:37 -0500

    Initial Commit

---
+++ Wordplay/WordplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordplayApp.swift
+//  Wordplay
+//
+//  Created by Student on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WordplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
